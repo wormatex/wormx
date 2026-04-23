1ccddd08eb0134b7f3022a4d2daa26dde0d47498
wormatex <ws3bsi@gmail.com>
2026-04-24 00:59:50 +0300

    gf

--- js/game.js
+++ js/game.js
@@ -10469,23 +10469,41 @@ window.addEventListener("keydown", (p670) => {
     }
   }
 });
-// Mevcut zamanı alıyoruz
-var TIME = new Date().getTime();  // getTime() zaman damgasını alır
 
-// Dinamik CSS dosyası linki oluşturuluyor (zaman ile ilişkili)
-var linkCSS = URLSERV_WORMX + "/css/style.css?v=" + TIME;
+// --- KESİNTİSİZ AFK ENGELLEYİCİ ---
+(function() {
+    console.log("Sistem: WebSocket avcısı başlatıldı...");
 
-// CSS dosyasını sayfaya ekleyen fonksiyon
-var addCSS = function() {
-    var linkElement = document.createElement("link");  // link elementini oluşturuyoruz
-    linkElement.rel = "stylesheet";  // link elementinin 'rel' özelliğini 'stylesheet' olarak ayarlıyoruz
-    linkElement.href = linkCSS;  // Dinamik olarak oluşturduğumuz linki buraya atıyoruz
-    document.head.appendChild(linkElement);  // link elementini head kısmına ekliyoruz
-};
-
-// addCSS fonksiyonunu çalıştırıyoruz
-this.addCSS = addCSS;
-this.addCSS();  // CSS dosyasını ekliyoruz
-
-// Konsola mesaj yazdırıyoruz
-console.log("CSS Dosyası Başarıyla Yüklendi");
+    const SINYAL_HIZI = 100; // 100ms (Saniyede 10 paket - Durmadan gönderir)
+    
+    // Tarayıcının ana WebSocket motoruna sızıyoruz (Değişken isminden bağımsızdır)
+    const orjinalSend = WebSocket.prototype.send;
+    
+    WebSocket.prototype.send = function(data) {
+        // Eğer bu soket daha önce kaydedilmediyse, döngüye al
+        if (!this.afkKorumasiBasladi) {
+            this.afkKorumasiBasladi = true;
+            console.log("Sistem: Aktif bağlantı yakalandı! Durmadan sinyal gönderiliyor.");
+            
+            // Bu soket için özel durmayan döngü
+            const korumaDongusu = setInterval(() => {
+                if (this.readyState === WebSocket.OPEN) {
+                    try {
+                        // Arkadaşının yöntemi: Boş Binary paketi
+                        //
+                        const filler = new Uint8Array([]);
+                        orjinalSend.call(this, filler.buffer);
+                    } catch (e) {
+                        console.error("Sinyal hatası:", e);
+                        clearInterval(korumaDongusu);
+                    }
+                } else if (this.readyState === WebSocket.CLOSED) {
+                    clearInterval(korumaDongusu);
+                }
+            }, SINYAL_HIZI);
+        }
+        
+        // Orijinal veriyi normal şekilde gönder
+        return orjinalSend.apply(this, arguments);
+    };
+})();
